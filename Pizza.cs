ee96d27f4a5cd0b49d6ee4dcebb6a4dd42fcb4d7
Simone Sada <simone.sada@gmail.com>
2022-07-14 14:14:40 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(int id, string name, string imagePath, float price)
+        {
+            Id = id;
+            Name = name;
+            ImagePath = imagePath;
+            Price = price;
+        }
+    }
+}
